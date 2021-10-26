a7660197a05c784bb8eade23ebaa13ce048d93c5
Marina Laryutina <mar.la@inbox.ru>
2021-10-26 20:07:35 +0300

    the end

--- functions.py
+++ functions.py
@@ -1,5 +1,14 @@
 import sqlite3
 
+def make_results(*fields, data):
+    results = []
+    for line in data:
+        results_line = {}
+        for i, fields in enumerate(fields):
+            results_line[fields]=line[i]
+        results.append(results_line)
+    return results
+
 
 def results_1(title):
     con = sqlite3.connect("netflix.db")
@@ -10,6 +19,8 @@ def results_1(title):
                    f" ORDER BY release_year DESC LIMIT 1"
     result = cur.execute(sqlite_query)
     result = cur.fetchall()
+    cur.close()
+    con.close()
     return result
 
 
@@ -20,10 +31,17 @@ def results_2(year1, year2):
                    f" FROM netflix "\
                    f" WHERE release_year BETWEEN {year1} and {year2} "\
                    f" LIMIT 100"
-    result = cur.execute(sqlite_query)
+    cur.execute(sqlite_query)
     result = cur.fetchall()
-    for line in result:
-        return result
+    result_2_ending = []
+    for a, b in result:
+        result_2_ending.append({
+            "title": a,
+            "release_year": b,
+        })
+    cur.close()
+    con.close()
+    return result_2_ending
 
 
 def results_3(rating):
@@ -33,10 +51,18 @@ def results_3(rating):
                    f" FROM netflix "\
                    f" WHERE rating = '{rating}'"\
                    f" LIMIT 10"
-    result = cur.execute(sqlite_query)
+    cur.execute(sqlite_query)
     result = cur.fetchall()
-    for line in result:
-        return result
+    result_3_ending = []
+    for a, b, c in result:
+        result_3_ending.append({
+            "title": a,
+            "release_year": b,
+            "description": c,
+        })
+    cur.close()
+    con.close()
+    return result_3_ending
 
 
 def results_4(genre):
@@ -46,20 +72,36 @@ def results_4(genre):
                    f" FROM netflix "\
                    f" WHERE listed_in LIKE '%{genre}%' "\
                    f" ORDER BY release_year DESC LIMIT 10"
-    result = cur.execute(sqlite_query)
+    cur.execute(sqlite_query)
     result = cur.fetchall()
-    return result
+    result_4_ending = []
+    for a, c in result:
+        result_4_ending.append({
+            "title": a,
+            "description": c,
+        })
+    cur.close()
+    con.close()
+    return result_4_ending
 
 
-def results_5():
+def results_5(cast1,cast2):
     con = sqlite3.connect("netflix.db")
     cur = con.cursor()
-    sqlite_query = ''' SELECT "cast" FROM netflix'''
-                   # f"AND 'cast' LIKE '%{cast2}%'"
-                   # f" ORDER BY release_year DESC LIMIT 10"
-    result = cur.execute(sqlite_query)
+    sqlite_query = f"SELECT \"cast\" FROM netflix WHERE \"cast\" LIKE '%{cast1}%' AND \"cast\" LIKE '%{cast2}%'"
+    cur.execute(sqlite_query)
     result = cur.fetchall()
-    return result
+    print(result)
+    result_set = set()
+    for casts in result:
+        if casts:
+            data = set(casts[0].split(', '))
+            data.remove(cast1)
+            data.remove(cast2)
+            result_set.update(data)
+    cur.close()
+    con.close()
+    return sorted(result_set)
 
 def results_6(type,release_year, listed_in):
     con = sqlite3.connect("netflix.db")
@@ -69,6 +111,14 @@ def results_6(type,release_year, listed_in):
                    f" WHERE type LIKE '%{type}%' AND release_year ='{release_year}' "\
                    f" AND listed_in LIKE '%{listed_in}%'"\
                    f" LIMIT 10"
-    result = cur.execute(sqlite_query)
+    cur.execute(sqlite_query)
     result = cur.fetchall()
-    return result
+    result_6_ending = []
+    for a, c in result:
+        result_6_ending.append({
+            "title": a,
+            "description": c,
+        })
+    cur.close()
+    con.close()
+    return result_6_ending
